1f8c79cf12559aee12a765b43892ceda36f5660b
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-27 15:19:17 -0500

    progress on salaries

---
+++ employee.js
@@ -0,0 +1,18 @@
+class Employee {
+    constructor(name, salary, title, manager = null) {
+        this.name = name;
+        this.salary = salary;
+        this.title = title;
+        this.manager = manager;
+    }
+}
+
+const leo = new Employee(
+    "Leonardo",
+    90000,
+    "Ninja",
+)
+
+console.log(leo);
+
+module.exports = Employee;
