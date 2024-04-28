0d74ad099192e0ef0828b6e516b07b21e35e3b80
Ruben Cuello <kuelmusic@gmail.com>
2024-04-28 16:31:18 -0400

    a;ld

--- employee.js
+++ employee.js
@@ -1,11 +1,22 @@
+// const Manager = require("./manager");
+
 class Employee {
-    constructor(name, salary, title, manager = null) {
+    constructor(name, salary, title, manager) {
         this.name = name;
         this.salary = salary;
         this.title = title;
-        this.manager = manager;
+        // this is a ternary that checks if there was a manager passed into the constructor
+        // if it wasn't it will equal null kind of like a regular ternary the right side is false
+        this.manager = manager ?? null;
+        if(this.manager){
+            this.manager.addEmployee(this)
+        }
     }
+
+
+
 }
+
 
 const leo = new Employee(
     "Leonardo",
--- manager.js
+++ manager.js
@@ -8,6 +8,7 @@ class Manager extends Employee {
 
     addEmployee(employee) {
         this.employees.push(employee);
+        
     }
 }
 
